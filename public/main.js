71954f0afed0dcc0374a3900fbfbb430e950f210
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-03-01 15:53:45 +0530

    backend update

--- public/main.js
+++ public/main.js
@@ -1,17 +1,30 @@
+var container = document.getElementById('container');
+const username = prompt("Please enter your name:");
 $(function () {
-    const socket = io();
+  const socket = io();
 
-    // Prompt the user for their name
-    const username = prompt("Please enter your name:");
-    socket.emit('setUsername', username);
+  socket.emit("setUsername", username);
 
-    $('form').submit(function () {
-        socket.emit('chat message', $('#input').val());
-        $('#input').val('');
-        return false;
-    });
+  $("form").submit(function () {
+    socket.emit("chat message", $("#input").val());
+    $("#input").val("");
+    return false;
+  });
 
-    socket.on('chat message', function (msg) {
-        $('#messages').append($('<li>').text(msg));
-    });
+  socket.on("chat message", function (msg) {
+    console.log(msg);
+    const a = msg.split(":");
+    console.log(a[1]);
+
+    var newMessage = document.createElement('div');
+    if (a[0] == username) {
+        newMessage.className = 'message right';
+    } else {
+        newMessage.className = 'message left';
+    }
+    newMessage.textContent = a[0] + ' : ' + a[1];
+    var container = document.getElementById('container'); // Replace 'yourContainerId' with the actual ID of your container
+    container.appendChild(newMessage);
+});
+
 });
